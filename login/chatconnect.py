d3cbf374dc0233d36723d5dcaaf7809627efbe99
Diego Fernandes <diego.fernandes@enjoei.com.br>
2021-04-15 22:28:00 -0300

    update system

--- login/chatconnect.py
+++ login/chatconnect.py
@@ -1,46 +1,52 @@
+
 import threading
 import socket
 import DataBaser
-
 from tkinter import *
 
-janelaChat = Tk()
-janelaChat.geometry("700x350")
-janelaChat.configure(background="white")
+class ChatConnect:
 
-labelEnterServer = Label(janelaChat, text="Conenctar ao server:", font=("Century Gothic", 12), bg="white", fg="black")
-labelEnterServer.place(x=120,y=110)
-InsertServer = Entry(janelaChat,width=28)
-InsertServer.place(x=300,y=112)
+    
 
 
+    janelaChat = Tk()
+    janelaChat.geometry("700x300")
+    janelaChat.configure(background="white")
 
-def EnterRoom():
-  nickname = "Cliente"
+    labelEnterServer = Label(janelaChat, text="Conenctar ao server:", font=("Century Gothic", 12), bg="white", fg="black")
+    labelEnterServer.place(x=120,y=110)
 
-  nameConnection = InsertServer.get()
-
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  client.connect((nameConnection, 55555))
-
-  def receive():
-    while True:
-        try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
-            message = client.recv(1024).decode("ascii")
-            if message == "NICK":
-                client.send(nickname.encode("ascii"))
-            else:
-                print(message)
-        except:
-            # Close Connection When Error
-            print("Ocorreu um erro!")
-            client.close()
-            break
-
-Room = Button(janelaChat, text="Entrar na Sala", command=EnterRoom)
-Room.place(x=300,y=150)
+    InsertServer = Entry(janelaChat,width=28)
+    InsertServer.place(x=300,y=112)
 
 
-janelaChat.mainloop()
+
+    def EnterRoom():
+      nickname = "Cliente"
+
+      nameConnection = InsertServer.get()
+
+      client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      client.connect((nameConnection, 55555))
+
+      def receive():
+        while True:
+            try:
+                # Receive Message From Server
+                # If 'NICK' Send Nickname
+                message = client.recv(1024).decode("ascii")
+                if message == "NICK":
+                    client.send(nickname.encode("ascii"))
+                else:
+                    print(message)
+            except:
+                # Close Connection When Error
+                print("Ocorreu um erro!")
+                client.close()
+                break
+
+    Room = Button(janelaChat, text="Entrar na Sala", command=EnterRoom)
+    Room.place(x=300,y=150)
+
+
+    janelaChat.mainloop()
